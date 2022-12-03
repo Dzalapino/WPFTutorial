08fe56e941523e0ff8b8479cb429f44d296ae0b1
Jakub Podkomorka <jpodkomorka@sii.pl>
2022-12-04 00:39:45 +0100

    done with the text block for now

--- WPFTutorial/WPFTutorial/MainWindow.xaml.cs
+++ WPFTutorial/WPFTutorial/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WPFTutorial
 {
@@ -23,6 +14,40 @@ namespace WPFTutorial
         public MainWindow()
         {
             InitializeComponent();
+
+            // Manipulate referenced textblock's properties
+            theLastOfTextBlocks.Text = "Hello from the cs side!";
+            theLastOfTextBlocks.Foreground = Brushes.Blue;
+
+            // Add new items from code and add the new textblock to canvas
+            Canvas canvas = new Canvas();
+            TextBlock textBlockFromCode = new TextBlock();
+            textBlockFromCode.Text = "Wow! You just created textblock from code. Nice.";
+            textBlockFromCode.TextWrapping = TextWrapping.Wrap;
+
+            // Append new text to the textblock by using Inlines.Add method
+            textBlockFromCode.Inlines.Add(" And this is added using Inlines!");
+
+            // Append new text with other formatting
+            textBlockFromCode.Inlines.Add(new Bold(new Italic(new Run("\nNew text added using inlines but with cool formatting!")
+            {
+                FontFamily = new FontFamily("Comic Sans MS"),
+                Foreground = (Brush)new BrushConverter().ConvertFromString("#d03753"),
+                TextDecorations = TextDecorations.Underline
+            })));
+
+            // Add the items to the children collection of the window elements to make them visible
+            canvas.Children.Add(textBlockFromCode);
+            mainGrid.Children.Add(canvas);
+
+            // position the textblock
+            Canvas.SetLeft(textBlockFromCode, 10);
+            Canvas.SetTop(textBlockFromCode, 250);
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.Uri.AbsoluteUri);
         }
     }
 }
