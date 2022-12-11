1f12c1eda43de26c16d430776190e28f4ea24cad
Jakub Podkomorka <jpodkomorka@sii.pl>
2022-12-11 15:51:03 +0100

    Finished buttons and events part of the tutorial

--- WPFTutorial/WPFTutorial/MainWindow.xaml.cs
+++ WPFTutorial/WPFTutorial/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WPFTutorial
 {
@@ -23,6 +12,29 @@ namespace WPFTutorial
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MyButton_Click(object sender, RoutedEventArgs e)
+        {
+            var rnd = new Random();
+            myLabel.Foreground = new SolidColorBrush(Color.FromArgb(255, (byte)rnd.Next(256), (byte)rnd.Next(256), (byte)rnd.Next(256)));
+            ++myLabel.FontSize;
+        }
+
+        private void MyButton_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            myLabel.Foreground = Brushes.Black;
+            --myLabel.FontSize;
+        }
+
+        private void MyButton_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            myButton.BorderThickness = new Thickness(4, 4, 4, 4);
+        }
+
+        private void MyButton_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            myButton.BorderThickness = new Thickness(2, 2, 2, 2);
         }
     }
 }
