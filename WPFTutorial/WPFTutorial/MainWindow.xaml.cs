ac9317884861cbd03753cfda0e313b657799c7a2
Jakub Podkomorka <jpodkomorka@sii.pl>
2023-01-07 14:44:43 +0100

    Finished that part of the tutorial

--- WPFTutorial/WPFTutorial/MainWindow.xaml.cs
+++ WPFTutorial/WPFTutorial/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace WPFTutorial
 {
@@ -23,6 +10,15 @@ namespace WPFTutorial
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void mySlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            /*            if (myTextBlock != null)
+                        {
+                            myTextBlock.FontSize = mySlider.Value;
+                            myTextBlock.Text = "This is the font size: " + mySlider.Value.ToString();
+                        }*/
         }
     }
 }
